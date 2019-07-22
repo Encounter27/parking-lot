3b69ff2ea8e34d312230d4be25b429ca46f35402
PRADIP ACHARJEE <pacharjee@slb.com>
2019-07-23 00:51:02 +0530

    Added unit tests for  commands

--- src/parking-lot-app/parking/lot_test.go
+++ src/parking-lot-app/parking/lot_test.go
@@ -54,3 +54,67 @@ func Test_lot_GetTotal(t *testing.T) {
 		}
 	})
 }
+
+func Test_getSlotType(t *testing.T) {
+	type args struct {
+		t CarType
+	}
+	tests := []struct {
+		name string
+		args args
+		want SlotType
+	}{
+		{"test 1", args{CAR}, DEFAULT},
+		{"test 1", args{MOTORBIKE}, TWO_WHEELER},
+		{"test 1", args{SEDAN}, FOUR_WHEELER},
+		{"test 1", args{SUV}, FOUR_WHEELER},
+		{"test 1", args{TRUCK}, BIG_VEHIHLE},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := getSlotType(tt.args.t); got != tt.want {
+				t.Errorf("getSlotType() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestAssignTicket(t *testing.T) {
+	tmp := `{"parking_name": "phoenix mall parking","floors":[{"total":2,"space":[{"from":1,"to":2,"type":"DEFAULT"}]}]}`
+	var cfg Config
+	json.Unmarshal([]byte(tmp), &cfg)
+
+	AddNewParking(cfg)
+
+	type args struct {
+		parkingName string
+		car         IVehicle
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{
+			"test assign parking",
+			args{
+				parkingName: "phoenix mall parking",
+				car: &Car{
+					Type:   CAR,
+					RegID:  "hdsvh",
+					Colour: "white",
+				},
+			},
+			false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := AssignTicket(tt.args.parkingName, tt.args.car)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AssignTicket() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+		})
+	}
+}
